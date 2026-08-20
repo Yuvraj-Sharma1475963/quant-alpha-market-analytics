f6dfdca69f1c251e06f01de1cd5d88951e803194
Yuvraj Sharma <yubraj002net@gmail.com>
2026-08-20 20:56:37 +0530

    Phase 2: cleans prices and computed daliy returns

---
+++ src/returns.py
@@ -0,0 +1,31 @@
+import pandas as pd 
+
+def load_prices(path="data/prices.csv"):
+
+
+    #loading data 
+    prices=pd.read_csv(path,index_col=0,parse_dates=True)
+    print(prices)
+    return prices
+
+
+def clean_prices(prices):
+    #cleaning the column which does not have any price data
+    prices=prices.dropna(axis=1,how="all") # how="all" means that drop the column only if all its value is nan
+
+    prices=prices.ffill()
+
+    return prices
+
+def compute_returns(prices):
+    returns=prices.pct_change()
+    returns=returns.dropna(how="all")
+    return returns
+
+if __name__ == "__main__":
+    prices=load_prices()
+    prices=clean_prices(prices)
+    returns=compute_returns(prices)
+
+    print("Returns shape: ",returns.shape)
+    print(returns.tail())
